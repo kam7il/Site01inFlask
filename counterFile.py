967d86b6cd5659fc2865b245be4b1409778a5d58
Kam7il <118853875+kam7il@users.noreply.github.com>
2023-08-15 23:00:38 +0200

    update

--- counterFile.py
+++ counterFile.py
@@ -19,4 +19,5 @@ def load_counter_file():
 # zapisanie stanu licznika do pliku
 def save_counter_file(counter):
     with open(counterFilePath, mode="w", encoding="utf-8") as file:
-        file.write(str(counter))
+        file.write(str(counter) + "\n")
+
